1ad3de30c298b012cb21cdf9bc6b2f9269198a5e
Manuel Schmid <manuel_schmid@hotmail.com>
2024-12-01 20:54:30 +0100

    day one task one

---
+++ 2024/01/01.c
@@ -0,0 +1,6 @@
+/* Advent of Code - Day 01
+ * Input: two lists of numbers
+ * Process: take n-th largest number of both lists and calculate difference between them
+ * Output: sum of differences
+ */
+
