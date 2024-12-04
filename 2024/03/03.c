adf680eae36fef592444a7f8aab1cfab5cb972d6
Schmid Manuel (shhm) <manuel_schmid@hotmail.com>
2024-12-04 07:54:57 +0100

    03 code structure

---
+++ 2024/03/03.c
@@ -0,0 +1,35 @@
+/* Advent of Code - Day 03
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// minput is a massive string
+// look for "mul(aaa,bbb)" strings
+// a and b are one to three digit numbers
+// any partial results are to be discarded
+// for every string a and b must be multiplied
+// the results are summed and that is the puzzle's answer
+
+int main() {
+    FILE *fptr;
+    fptr = fopen("input.txt", "r");
+
+    if (fptr == NULL) {
+        fclose(fptr);
+        return 1; // file doesn't exist
+    }
+    
+    char input_char;
+    int ans;
+
+    while (input_char = fgetc(fptr) != EOF) {
+
+    }
+    fclose(fptr);
+
+    printf("\n\nAnswers: %d\n", ans);
+
+    return 0;
+}
