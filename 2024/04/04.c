408e5c423d921c2b0450427c42279ddb3392aabd
Schmid Manuel (shhm) <manuel_schmid@hotmail.com>
2024-12-04 09:26:05 +0100

    game plan

--- 2024/04/04.c
+++ 2024/04/04.c
@@ -21,12 +21,70 @@ int main() {
     int input_char;
     int ans = 0;
 
-
+    // horizontal, forward
     while ((input_char = fgetc(fptr)) != EOF) {
         //putchar(input_char);
 
 
     }
+    rewind(fptr);
+
+    // horizontal, backward
+    while ((input_char = fgetc(fptr)) != EOF) {
+        //putchar(input_char);
+
+
+    }
+    rewind(fptr);
+
+    // vertical, forward
+    while ((input_char = fgetc(fptr)) != EOF) {
+        //putchar(input_char);
+
+
+    }
+    rewind(fptr);
+
+    // vertical, backward
+    while ((input_char = fgetc(fptr)) != EOF) {
+        //putchar(input_char);
+
+
+    }
+    rewind(fptr);
+
+    // diagonal left-right, forward
+    while ((input_char = fgetc(fptr)) != EOF) {
+        //putchar(input_char);
+
+
+    }
+    rewind(fptr);
+
+    // diagonal left-right, backward
+    while ((input_char = fgetc(fptr)) != EOF) {
+        //putchar(input_char);
+
+
+    }
+    rewind(fptr);
+
+    // diagonal right-left, forward
+    while ((input_char = fgetc(fptr)) != EOF) {
+        //putchar(input_char);
+
+
+    }
+    rewind(fptr);
+
+    // diagonal right-left, backward
+    while ((input_char = fgetc(fptr)) != EOF) {
+        //putchar(input_char);
+
+
+    }
+    rewind(fptr);
+    
     fclose(fptr);
 
     printf("\n\nAnswers: %d\n", ans);
