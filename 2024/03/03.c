d1cc201401a8270855bb20e3aeda05216411bc23
Schmid Manuel (shhm) <manuel_schmid@hotmail.com>
2024-12-04 08:58:36 +0100

    don't work yet

--- 2024/03/03.c
+++ 2024/03/03.c
@@ -22,10 +22,112 @@ int main() {
     }
     
     int input_char;
-    int ans;
+    unsigned long ans = 0;
+
+    /* States:
+        0: M
+        1: U
+        2: L
+        3: (
+        4: n1
+        5: ,
+        6: n2
+        7: )
+    */
+    int state = 0;
+
+    int n1, n2;
+    int digit_count;
 
     while ((input_char = fgetc(fptr)) != EOF) {
-        putchar(input_char);
+        //putchar(input_char);
+        if (state == 0) {
+            n1 = -1;
+            n2 = -1;
+            digit_count = 0;
+        }
+
+        switch (input_char)
+        {
+        case 'm':
+            if (state == 0)
+                state++;
+            else
+                state = 0;
+            break;
+
+        case 'u':
+            if (state == 1)
+                state++;
+            else
+                state = 0;
+            break;
+
+        case 'l':
+            if (state == 2)
+                state++;
+            else
+                state = 0;
+            break;
+
+        case '(':
+            if (state == 3)
+                state++;
+            else
+                state = 0;
+            break;
+
+        case ',':
+            if (state == 5 || state == 4)
+                state++;
+            else
+                state = 0;
+            break;
+
+        case ')':
+            if (state == 7 || state == 6) {
+                if (n1 < 0 || n2 < 0) {
+                    state = 0;
+                    break;
+                }
+                ans += n1 * n2;
+                printf("%d,%d\n", n1, n2);
+            }
+            state = 0;
+            break;
+
+        default:
+            if ((state == 4 || state == 6) && input_char >= 0x30 && input_char <= 0x39) {
+                if (state == 4) {
+                    if (n1 < 0) {
+                        n1 = 0;
+                        digit_count = 0;
+                    }
+
+                    n1 = (input_char - 0x30) + n1*10;
+                    digit_count++;
+                    if (digit_count >= 3) {
+                        state++;
+                        digit_count = 0;
+                    }
+                } else if (state == 6) {
+                    if (n2 < 0) {
+                        n2 = 0;
+                        digit_count = 0;
+                    }
+
+                    n2 = (input_char - 0x30) + n2*10;
+                    digit_count++;
+                    if (digit_count >= 3) {
+                        state++;
+                        digit_count = 0;
+                    }
+                }
+            } else {
+                state = 0;
+            }
+            break;
+        }
     }
     fclose(fptr);
 
