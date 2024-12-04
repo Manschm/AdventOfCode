0ba4bb11da67964c3c424a87f2a772c51db7bbdb
Manuel Schmid <manuel_schmid@hotmail.com>
2024-12-04 15:51:06 +0100

    second try of 2071 is also too low

--- 2024/04/04.c
+++ 2024/04/04.c
@@ -23,98 +23,88 @@ int main() {
     }
     
     int input_char;
+    int ans = 0;
 
     /* State:
-     * 0: X
-     * 1: M
-     * 2: A
-     * 3: S
+     * V  F  B
+     * 0  X  S
+     * 1  M  A
+     * 2  A  M
+     * 3  S  X
     */
-    int state = 0;
+    int state_f = 0;
+    int state_b = 0;
 
-    // horizontal, forward
-    int ans1 = 0;
-    while ((input_char = fgetc(fptr)) != EOF) {
-        switch (input_char)
-        {
-        case 'X':
-            state = 1;
-            break;
-            
-        case 'M':
-            if (state == 1)
-                state++;
-            else
-                state = 0;
-            break;
-            
-        case 'A':
-            if (state == 2)
-                state++;
-            else
-                state = 0;
-            break;
-            
-        case 'S':
-            if (state == 3) {
-                ans1++;
-            }
-            state = 0;
-            break;
-        
-        default:
-            state = 0;
-            break;
-        }
-    }
-    rewind(fptr);
-    printf("Horizontal forward: %d\n", ans1);
+    int state = 0; //TODO: remove
 
-    // horizontal, backward
-    int ans2 = 0;
-    while ((input_char = fgetc(fptr)) != EOF) {
-        switch (input_char)
-        {
-        case 'S':
-            state = 1;
-            break;
-            
-        case 'A':
-            if (state == 1)
-                state++;
-            else
-                state = 0;
-            break;
-            
-        case 'M':
-            if (state == 2)
-                state++;
-            else
-                state = 0;
-            break;
-            
-        case 'X':
-            if (state == 3) {
-                ans2++;
-            }
-            state = 0;
-            break;
-        
-        default:
-            state = 0;
-            break;
-        }
-    }
-    rewind(fptr);
-    printf("Horizontal backward: %d\n", ans2);
-
+    // create matrix
     char input_line[LINELEN];
-    for (int row = 0; fgets(input_line, LINELEN, fptr) != NULL; row++) {
-        for (int col = 0; input_line[col] != '\n' && input_line[col] != '\0'; col++) {
+    int row, col;
+    for (row = 0; fgets(input_line, LINELEN, fptr) != NULL; row++) {
+        for (col = 0; input_line[col] != '\n' && input_line[col] != '\0'; col++) {
             char_matrix[row][col] = input_line[col];
         }
     }
+    printf("row: %d, col: %d\n", row, col);
     fclose(fptr);
+
+    // horizontal, forward & backward
+    for (int row = 0; row < FILELEN; row++) {
+        for (int col = 0; col < FILELEN; col++) {
+            switch (char_matrix[row][col])
+            {
+            case 'X':
+                state_f = 1;
+
+                if (state_b == 3)
+                    ans++;
+                state_b = 0;
+
+                break;
+                
+            case 'M':
+                if (state_f == 1)
+                    state_f++;
+                else
+                    state_f = 0;
+
+                if (state_b == 2)
+                    state_b++;
+                else
+                    state_b = 0;
+
+                break;
+                
+            case 'A':
+                if (state_f == 2)
+                    state_f++;
+                else
+                    state_f = 0;
+
+                if (state_b == 1)
+                    state_b++;
+                else
+                    state_b = 0;
+
+                break;
+                
+            case 'S':
+                if (state_f == 3)
+                    ans++;
+                state_f = 0;
+
+                state_b = 1;
+
+                break;
+            
+            default:
+                state_f = 0;
+                state_b = 0;
+                break;
+            }
+        }
+    }
+    printf("Horizontal: %d\n", ans);
 
     // vertical, forward
     int ans3 = 0;
@@ -395,7 +385,7 @@ int main() {
     printf("Diagonal right-left backward: %d\n", ans8);
     
 
-    printf("\n\nAnswers: %d\n", ans1+ans2+ans3+ans4+ans5+ans6+ans7+ans8);
+    printf("\n\nAnswers: %d\n", ans+ans3+ans4+ans5+ans6+ans7+ans8);
 
     return 0;
 }
